20d12d2da75e0c2f3a7782796b68f37720c8ff02
Jeffry Almonte <djempsly120@gmail.com>
2024-05-23 16:31:11 -0400

    Modificando los datos del componente de Acerca de Nosotros

--- src/Pages/SobreNosotros/index.js
+++ src/Pages/SobreNosotros/index.js
@@ -3,6 +3,76 @@ import React from "react";
 function SobreNosotros(){
     return(
         <>
+        <div className="container-servicio">
+            <div className="mision">
+                <h2>Misión</h2>
+                    <p>
+                
+
+                    Nuestra misión es proporcionar a nuestros clientes  <br/>
+                    productos tecnológicos de alta calidad, personalizados  <br/>
+                    según sus necesidades y preferencias. Nos esforzamos por  <br/>
+                    ofrecer una experiencia de compra única y satisfactoria,  <br/>
+                    combinando innovación, diseño y funcionalidad en cada  <br/>
+                    uno de nuestros artículos.
+                    </p>
+            </div>
+            <div>
+                <h2> Visión</h2>
+                    <p>
+                        Ser una empresa multinacional líder  en servicios y productos <br/>
+                        de excelente calidad,
+
+                        Ser líderes en el mercado de la personalización de productos <br/>
+                        tecnológicos, reconocidos por nuestra excelencia en servicio al <br/>
+                        cliente, calidad de nuestros productos y nuestra capacidad para <br/>
+                        adaptarnos a las tendencias y demandas del mercado. Aspiramos a <br/>
+                        expandir nuestra presencia global y convertirnos en la opción <br/>
+                        preferida para aquellos que buscan productos tecnológicos personalizados.
+
+                 </p>
+            </div>
+
+            <div>
+                <h2>Valores</h2>
+                <p>
+            
+                    Innovación: Nos comprometemos a estar a la vanguardia de la tecnología, <br/>
+                     ofreciendo productos innovadores y soluciones creativas. <br/><br/>
+                    Calidad: La calidad es nuestra prioridad en todos los aspectos de <br/>
+                    nuestra operación, desde la selección de productos hasta el servicio al cliente. <br/> <br/> 
+                    Personalización: Valoramos la individualidad de nuestros clientes y nos <br/>
+                    esforzamos por ofrecer productos que reflejen sus gustos y necesidades únicas. <br/> <br/>
+                    Servicio al Cliente: Creemos en un servicio al cliente excepcional, <br/>
+                    proporcionando soporte amigable, profesional y eficiente. <br/> <br/> 
+                    Sostenibilidad: Nos comprometemos a prácticas empresariales sostenibles <br/> 
+                    que minimicen nuestro impacto ambiental y promuevan el bienestar de la comunidad. <br/> <br/>
+
+
+                </p>
+            </div>
+
+            <div>
+                <h2> Metas</h2>
+                <p>
+
+                Expandir la Oferta de Productos: Ampliar nuestra línea de productos tecnológicos 
+                personalizados para incluir las últimas innovaciones y tendencias del mercado.
+
+Mejorar la Experiencia del Cliente: Implementar nuevas tecnologías y 
+procesos que faciliten la personalización y compra de nuestros productos, mejorando la satisfacción del cliente.
+Aumentar la Presencia en el Mercado: Expandir nuestra presencia en mercados nacionales e internacionales a través de estrategias de marketing efectivas y colaboraciones estratégicas.
+Fomentar la Innovación Continua: Invertir en investigación y desarrollo para seguir innovando y ofreciendo productos únicos y de alta calidad.
+Compromiso con la Sostenibilidad: Adoptar prácticas más sostenibles en nuestra cadena de suministro y operaciones diarias para reducir nuestro impacto ambiental.
+                </p>
+            </div>
+
+
+
+
+
+
+        </div>
         
         </>
     )
